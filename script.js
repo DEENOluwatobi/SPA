ed97a0111992589c0f940a8398ce068cd1aabd06
DEENOluwatobi <shodimutobie@gmail.com>
2023-08-16 14:07:16 +0100

    section2 completed

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+    // SCROLL SPY --------------
     const navLinks = document.querySelectorAll(".menu ul li a");
     const sections = document.querySelectorAll("section");
 
@@ -42,32 +44,51 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
+    // ANIMATIONS ---------------
     const animatedElements = document.querySelectorAll(".animate-fade-right, .animate-fade-up, .animate-fade-down");
-        animatedElements.forEach(element => {
-            element.style.animationPlayState = "running";
+    animatedElements.forEach(element => {
+        element.style.animationPlayState = "running";
+    });
+
+    const exploreTitle = document.querySelector('.explore-title');
+    const exploreSection = document.querySelector('.explore-section');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                exploreTitle.classList.add('active');
+                observer.unobserve(exploreSection);
+            }
         });
+    }, { threshold: 1 });
+    observer.observe(exploreSection);
+
+    // SVG ANIMATE  -------------------
+    const svgContainer = document.querySelector('.svg-con');
+    const svgObserver = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                svgContainer.classList.add('active');
+                svgObserver.unobserve(section2);
+            }
+        });
+    }, { threshold: 1});
+    svgObserver.observe(section2);
     
 
+    // GALLERY SCROLLER ---------
+    const slider = document.querySelector(".slider__content");
+    const prevButton = document.querySelector(".slider__nav__button--prev");
+    const nextButton = document.querySelector(".slider__nav__button--next");
 
-    const gallery = document.querySelector(".gallery");
-    const prevButton = document.querySelector(".prev-btn");
-    const nextButton = document.querySelector(".next-btn");
+    function scrollLeft() {
+        slider.scrollBy({ left: -slider.offsetWidth, behavior: 'smooth' });
+    }
 
-    const scrollAmount = 350; 
+    function scrollRight() {
+        slider.scrollBy({ left: slider.offsetWidth, behavior: 'smooth' });
+    }
 
-    prevButton.addEventListener("click", function() {
-        gallery.scrollBy({
-            left: -scrollAmount,
-            behavior: "smooth"
-        });
-    });
-
-    nextButton.addEventListener("click", function() {
-        gallery.scrollBy({
-            left: scrollAmount,
-            behavior: "smooth"
-        });
-    });
-
-
+    prevButton.addEventListener("click", scrollLeft);
+    nextButton.addEventListener("click", scrollRight);
 });
