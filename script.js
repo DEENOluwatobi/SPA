39502a37ea66bee45969a6de97e25dad935d1f79
DEENOluwatobi <shodimutobie@gmail.com>
2023-08-16 10:09:42 +0100

    Gallery init

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     const navLinks = document.querySelectorAll(".menu ul li a");
+    const sections = document.querySelectorAll("section");
 
     navLinks.forEach(link => {
         link.addEventListener("click", event => {
             event.preventDefault();
 
-            // Toggle background color of the clicked link
-            link.classList.toggle("active");
-            
-            // Remove active class and background color from other links
             navLinks.forEach(otherLink => {
                 if (otherLink !== link) {
                     otherLink.classList.remove("active");
@@ -18,8 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
             const targetId = link.getAttribute("href");
             const targetSection = document.querySelector(targetId);
 
-            // Calculate the offset taking into account the fixed menu height
-            const offset = targetSection.getBoundingClientRect().top + window.scrollY - 60;
+            const offset = targetSection.getBoundingClientRect().top + window.scrollY;
 
             window.scrollTo({
                 top: offset,
@@ -27,13 +23,51 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         });
     });
-});
 
-document.addEventListener("DOMContentLoaded", function() {
-    AOS.init();
+    window.addEventListener("scroll", () => {
+        const currentScroll = window.scrollY;
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop - 50;
+
+            if (currentScroll >= sectionTop) {
+                const id = section.getAttribute("id");
+                navLinks.forEach(link => {
+                    link.classList.remove("active");
+                    if (link.getAttribute("href") === `#${id}`) {
+                        link.classList.add("active");
+                    }
+                });
+            }
+        });
+    });
 
     const animatedElements = document.querySelectorAll(".animate-fade-right, .animate-fade-up, .animate-fade-down");
-    animatedElements.forEach(element => {
-        element.style.animationPlayState = "running";
+        animatedElements.forEach(element => {
+            element.style.animationPlayState = "running";
+        });
+    
+
+
+    const gallery = document.querySelector(".gallery");
+    const prevButton = document.querySelector(".prev-btn");
+    const nextButton = document.querySelector(".next-btn");
+
+    const scrollAmount = 350; 
+
+    prevButton.addEventListener("click", function() {
+        gallery.scrollBy({
+            left: -scrollAmount,
+            behavior: "smooth"
+        });
     });
+
+    nextButton.addEventListener("click", function() {
+        gallery.scrollBy({
+            left: scrollAmount,
+            behavior: "smooth"
+        });
+    });
+
+
 });
